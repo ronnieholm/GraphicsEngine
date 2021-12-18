3946ef73dc9fed32c800b094deb817698c610f2b
Ronnie Holm <mail@bugfree.dk>
2021-12-18 14:35:46 +0100

    C# 10: File-scoped namespace

--- Graphics.Engine/Matrix.cs
+++ Graphics.Engine/Matrix.cs
@@ -11,114 +11,113 @@ using System.Text;
 //
 // We don't have a current need for anything but doubles.
 
-namespace Graphics.Engine
+namespace Graphics.Engine;
+
+public struct Matrix
 {
-    public struct Matrix
+    readonly double[,] _matrix;
+
+    public int Rows => _matrix.GetLength(0);
+    public int Columns => _matrix.GetLength(1);
+
+    public Matrix(int rows, int columns)
     {
-        readonly double[,] _matrix;
+        _matrix = new double[rows, columns];
+    }
 
-        public int Rows => _matrix.GetLength(0);
-        public int Columns => _matrix.GetLength(1);
+    public double this[int row, int column] 
+    {
+        get => _matrix[row, column];
+        set => _matrix[row, column] = value;
+    } 
 
-        public Matrix(int rows, int columns)
-        {
-            _matrix = new double[rows, columns];
-        }
+    // Data most be passed row-wise.
+    public Matrix(int rows, int columns, double[] data) : this(rows, columns)
+    {
+        if (data.Length != rows * columns)
+            throw new Exception("Too little data");
 
-        public double this[int row, int column] 
-        {
-            get => _matrix[row, column];
-            set => _matrix[row, column] = value;
-        } 
+        for (var i = 0; i < Rows; i++)
+        for (var j = 0; j < Columns; j++)
+            this[i, j] = data[i * Columns + j];
+    }
 
-        // Data most be passed row-wise.
-        public Matrix(int rows, int columns, double[] data) : this(rows, columns)
-        {
-            if (data.Length != rows * columns)
-                throw new Exception("Too little data");
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public double Get(int row, int column) => _matrix[row, column];
 
-            for (var i = 0; i < Rows; i++)
-                for (var j = 0; j < Columns; j++)
-                    this[i, j] = data[i * Columns + j];
-        }
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public double Get(int row, int column) => _matrix[row, column];
-
-        [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public void Set(int row, int column, double value) => _matrix[row, column] = value;
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public void Set(int row, int column, double value) => _matrix[row, column] = value;
         
-        public static Matrix operator+(Matrix a, Matrix b)
+    public static Matrix operator+(Matrix a, Matrix b)
+    {
+        if (a.Rows != b.Rows || a.Columns != b.Columns)
+            throw new Exception("Mismatched dimensions");
+
+        var r = new Matrix(a.Rows, a.Columns);
+        for (var i = 0; i < a.Rows; i++)
+        for (var j = 0; j < a.Rows; j++)
+            r[i, j] = a[i, j] + b[i, j];
+        return r;
+    }
+
+    public static Matrix operator-(Matrix a, Matrix b)
+    {
+        if (a.Rows != b.Rows || a.Columns != b.Columns)
+            throw new Exception("Mismatched dimensions");
+
+        var r = new Matrix(a.Rows, a.Columns);
+        for (var i = 0; i < a.Rows; i++)
+        for (var j = 0; j < a.Rows; j++)
+            r[i, j] = a[i, j] - b[i, j];
+        return r;
+    }
+
+    public static Matrix operator*(Matrix a, Matrix b)
+    {
+        if (a.Columns != b.Rows)
+            throw new Exception("Dimension mismatch");
+
+        var r = new Matrix(a.Rows, b.Columns);
+        for (var i = 0; i < a.Rows; i++)
         {
-            if (a.Rows != b.Rows || a.Columns != b.Columns)
-                throw new Exception("Mismatched dimensions");
-
-            var r = new Matrix(a.Rows, a.Columns);
-            for (var i = 0; i < a.Rows; i++)
-                for (var j = 0; j < a.Rows; j++)
-                    r[i, j] = a[i, j] + b[i, j];
-            return r;
-        }
-
-        public static Matrix operator-(Matrix a, Matrix b)
-        {
-            if (a.Rows != b.Rows || a.Columns != b.Columns)
-                throw new Exception("Mismatched dimensions");
-
-            var r = new Matrix(a.Rows, a.Columns);
-            for (var i = 0; i < a.Rows; i++)
-                for (var j = 0; j < a.Rows; j++)
-                    r[i, j] = a[i, j] - b[i, j];
-            return r;
-        }
-
-        public static Matrix operator*(Matrix a, Matrix b)
-        {
-            if (a.Columns != b.Rows)
-                throw new Exception("Dimension mismatch");
-
-            var r = new Matrix(a.Rows, b.Columns);
-            for (var i = 0; i < a.Rows; i++)
+            for (var j = 0; j < b.Columns; j++)
             {
-                for (var j = 0; j < b.Columns; j++)
-                {
-                    double cij = 0.0;
-                    for (var k = 0; k < a.Columns; k++)
-                        cij += a[i, k] * b[k, j];
-                    r[i, j] = cij;
-                }
+                double cij = 0.0;
+                for (var k = 0; k < a.Columns; k++)
+                    cij += a[i, k] * b[k, j];
+                r[i, j] = cij;
             }
-            return r;
         }
+        return r;
+    }
 
-        public static Matrix operator/(Matrix a, Matrix b)
+    public static Matrix operator/(Matrix a, Matrix b)
+    {
+        if (a.Rows != b.Rows || a.Columns != b.Columns)
+            throw new Exception("Mismatched dimensions");
+
+        var r = new Matrix(a.Rows, a.Columns);
+        for (var i = 0; i < a.Rows; i++)
+        for (var j = 0; j < a.Rows; j++)
+            r[i, j] = a[i, j] / b[i, j];
+        return r;
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        for (var i = 0; i < _matrix.GetLength(0); i++)
         {
-            if (a.Rows != b.Rows || a.Columns != b.Columns)
-                throw new Exception("Mismatched dimensions");
+            if (i != 0)
+                sb.Append("\n");
 
-            var r = new Matrix(a.Rows, a.Columns);
-            for (var i = 0; i < a.Rows; i++)
-                for (var j = 0; j < a.Rows; j++)
-                    r[i, j] = a[i, j] / b[i, j];
-            return r;
-        }
-
-        public override string ToString()
-        {
-            var sb = new StringBuilder();
-            for (var i = 0; i < _matrix.GetLength(0); i++)
+            for (var j = 0; j < _matrix.GetLength(1); j++)
             {
-                if (i != 0)
-                    sb.Append("\n");
-
-                for (var j = 0; j < _matrix.GetLength(1); j++)
-                {
-                    if (j != 0) 
-                        sb.Append(", ");
-                    sb.Append(this[i, j]);
-                }
+                if (j != 0) 
+                    sb.Append(", ");
+                sb.Append(this[i, j]);
             }
-            return sb.ToString();            
         }
+        return sb.ToString();            
     }
 }
